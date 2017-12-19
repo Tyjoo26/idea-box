b77589517d7bcbe7e77847f2f017075e32efbec8
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-19 11:37:30 -0700

    Add session destroy method for functionality for logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(user_name: params[:user_name])
+    # byebug
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
 
@@ -13,4 +14,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
 end
