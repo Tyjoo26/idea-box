514d9a80b0cb099d09104582d2b68349fd1b6854
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-19 16:06:53 -0700

    Create admin can create category spec, feature test passes

--- spec/features/admin_can_create_category_spec.rb
+++ spec/features/admin_can_create_category_spec.rb
@@ -2,8 +2,31 @@ require 'rails_helper'
 
 describe "As a verified Admin" do
   describe "When I go categories/new" do
-    xit "displays a form field that allows me to create a new category and redirects me to a category index page" do
-      
+    it "displays a form field that allows me to create a new category and redirects me to a category index page" do
+      admin = create(:user, role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      visit new_admin_category_path
+
+
+      fill_in"category[name]", with: "Terrible IDEAS"
+      click_on("Create Category")
+
+      expect(current_path).to eq(admin_category_path(Category.last.id))
+      expect(page).to have_content("Terrible IDEAS")
+    end
+  end
+end
+
+describe "As a default user" do
+  describe "when i go to categories new" do
+    xit "should display a 404 error" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit new_admin_category_path
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
