ac044cc0376bcc56b02d9f65ab900f36d83af0eb
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-18 17:45:38 -0700

    Build relatinship between user and idea at database level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218200740) do
+ActiveRecord::Schema.define(version: 20171219004445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 20171218200740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_ideas_on_category_id"
+    t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 20171218200740) do
   end
 
   add_foreign_key "ideas", "categories"
+  add_foreign_key "ideas", "users"
 end
