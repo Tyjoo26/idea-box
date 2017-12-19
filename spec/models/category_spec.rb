a238b29f73008278af8dcb94b3e12eef5b0ee795
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-19 16:17:32 -0700

    modified index for edit and creat category link functionality, created tests in category spec and user spec, test passes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it "cant save without a name" do
+      category = Category.new
+
+      expect(category).to_not be_valid
+    end
+
+    it 'can save with a name' do
+      category = Category.new(name: "Shit")
+
+      expect(category).to be_valid
+    end
+  end
 end
